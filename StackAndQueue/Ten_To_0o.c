e3820ebf93a009d778f243bb146e79eff635192e
TianXingSY <1460552465@qq.com>
2024-04-02 22:17:44 +0800

    修改

--- StackAndQueue/Ten_To_0o.c
+++ StackAndQueue/Ten_To_0o.c
@@ -2,15 +2,15 @@
 // Created by l1460 on 2024/4/2.
 //
 #include "SeqStack.c"
-//利用顺序栈实现十进制转换二进制
+//利用顺序栈实现十进制转换N进制
 
 
-int Ten_To_0o(int num) {
+int Ten_To_0o(int num, int n) {
     SqStack S;
     InitStack(&S);
     while (num) {
-        Push(&S, num % 2);
-        num /= 2;
+        Push(&S, num % n);
+        num /= n;
     }
     while (S.top!=-1) {
         Pop(&S, &num);
@@ -20,9 +20,11 @@ int Ten_To_0o(int num) {
 }
 
 int main() {
-    int num;
+    int num,n;
     printf("请输入一个十进制数：");
     scanf("%d", &num);
-    Ten_To_0o(num);
+    printf("请输入目标进制数：");
+    scanf("%d", &n);
+    Ten_To_0o(num,n);
     return 0;
 }
