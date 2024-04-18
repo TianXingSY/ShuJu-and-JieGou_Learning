df730142ebe659fb65aa324ad6985d07cf3bf83c
TianXingSY <1460552465@qq.com>
2024-04-18 09:41:47 +0800

    二叉树

---
+++ two-X-tree/bittree.c
@@ -0,0 +1,38 @@
+//
+// Created by l1460 on 24-4-18.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct treeNode {
+    int data;
+    struct treeNode *left;
+    struct treeNode *right;
+    struct treeNode *parent;
+}bittree;
+//二叉树前序建立
+bittree *createtree(bittree *root) {
+    char c;
+    c=getchar();
+    if (c == '#') {
+        return NULL;
+    }
+    else{
+        root = (bittree *) malloc(sizeof(bittree));
+        printf("请输入值：");
+        root->data=c;
+        root->left=createtree(root->left);
+        root->right=createtree(root->right);
+        return root;
+    }
+}
+
+int PreOrder(bittree *root) {
+    if (root == NULL) {
+        return 0;
+    }
+    printf("%c",root->data);
+    PreOrder(root->left);
+    PreOrder(root->right);
+    return 0;
+}
