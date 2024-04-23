bbfb9d9853e55f420875a51ad412762d46c3296c
TianXingSY <1460552465@qq.com>
2024-04-23 16:04:49 +0800

    bug

--- two-X-tree/bittree.c
+++ two-X-tree/bittree.c
@@ -26,6 +26,31 @@ bittree *createtree(bittree *root) {
         return root;
     }
 }
+//通过前序序列和中序序列建立二叉树
+bittree *createtree_by_PreandIn(bittree *root,char *Pre,char *Pin,int len) {
+    if (len == 0) {
+        return NULL;
+    }
+    else{
+        int left_len=0;
+        char *Pin_left;
+        root = (bittree *) malloc(sizeof(bittree));
+        root->data=Pre[0];
+        for (int i = 0; i < len; ++i) {
+            if (Pin[i] == Pre[0]) {
+                left_len = i;
+                break;
+            }
+        }
+        Pin_left=(char *)malloc(sizeof(char)*left_len);
+        for(int i = 0; i < left_len; ++i){
+            Pin_left[i]=Pin[i];
+        }
+        root->left=createtree_by_PreandIn(root->left,Pre+1,Pin_left,left_len);
+        root->right=createtree_by_PreandIn(root->right,Pre+left_len+1,Pin+left_len+1,len-left_len-1);
+        return root;
+    }
+}
 
 int PreOrder(bittree *root) {
     if (root == NULL) {
@@ -38,8 +63,13 @@ int PreOrder(bittree *root) {
 }
 
 int main(){
-    bittree *two;
-    two=createtree(two);
+    bittree *two,*miao;
+    char Pre[6]="ABDCEF";
+    char Pin[6]="DBACEF";
+    /*two=createtree(two);
     PreOrder(two);
+    printf("\n");*/
+    createtree_by_PreandIn(miao,Pre,Pin,6);
+    PreOrder(miao);
     return 0;
-};
+}
